7338af7f9ad0df9fc76651cda4c0141b7ebdb9e9
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-09-17 16:16:25 -0500

    added power table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -20,16 +20,39 @@ public class ControlFlowExercises {
 //        for(int i = 2; i <= 1000000; i *=Math.pow(i, 1)){
 //            System.out.println("i = " + i);
 //        }
-        for(int i = 1; i <=100; i++){
-            if(i % 15 == 0){
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0){
-                System.out.println("Fizz");
-            } else if(i % 5 == 0){
-                System.out.println("Buzz");
-            } else {
-                System.out.println("i = " + i);
+//        for(int i = 1; i <=100; i++){
+//            if(i % 15 == 0){
+//                System.out.println("FizzBuzz");
+//            } else if(i % 3 == 0){
+//                System.out.println("Fizz");
+//            } else if(i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println("i = " + i);
+//            }
+//        }
+//        System.out.println("Enter a int");
+//        int i = input.nextInt();
+        System.out.println("Enter a Intger");
+        int startValue = 1;
+        int inc = input.nextInt();
+
+        for (int row =0; row < inc; row++) {
+            int y = 1;
+
+            for (int col = startValue; col < startValue + 3; col++) {
+
+                y = y * startValue;
+                System.out.print(y + " ");
+
             }
+
+
+            System.out.println();
+            startValue++;
         }
+
+
+
     }
 }
