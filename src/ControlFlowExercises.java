420c65afdf0478d2a2f009ccf9bee7a20b358db7
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-09-17 15:16:02 -0500

    math pow loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int i = 100;
+//        do {
+//            System.out.println("i = " + i);
+//            i -=
+//        } while (i >= 0);
+            int i = 2;
         do {
-            System.out.println("i = " + i);
-            i -= 5;
-        } while (i >= 0);
+            System.out.println(i);
+            i *= Math.pow(i, 1);
 
+
+        }while(i <= 1000000);
     }
 }
