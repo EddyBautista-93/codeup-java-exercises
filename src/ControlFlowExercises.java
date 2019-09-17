7d91aa44af1ade726ee06d6354c644f6e5262309
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-09-17 14:53:16 -0500

    finnished first part of do while loop before alteration

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int i = 5;
-        while ( i <= 15 ){
-            System.out.println(i);
-            i++;
-        }
+        int i = 0;
+        do {
+            System.out.println("i = " + i);
+            i += 2;
+        } while (i <= 100);
 
     }
 }
