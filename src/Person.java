8a5341a8977923539ad22a785fb517aef58bdabe
Eddy Bautista <bautista.j.eddy@gmail.com>
2019-09-24 12:45:30 -0500

    exercise 2 completed

--- src/Person.java
+++ src/Person.java
@@ -20,7 +20,32 @@ public class Person {
 
     public static void main(String[] args) {
         Person eddy = new Person("eddy");
-        eddy.sayHello();
-        eddy.setName("eddy");
+//        eddy.sayHello();
+//        eddy.setName("eddy");
+
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+////        / will be true due to name value
+//        System.out.println(person1.getName().equals(person2.getName()));
+////        Will be false due to two diffrent instances
+//        System.out.println(person1 == person2);
+
+
+//        Equals as true because they are the same instance
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
+
     }
 }
